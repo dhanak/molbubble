2ebfd9f8588bbd120dc6584e200fed47cc337d00
David Hanak <dhanak@gmail.com>
2015-01-12 08:45:06 +0100

    Last version (unstable) before rewrite

--- src/mol_bubble.c
+++ src/mol_bubble.c
@@ -1,22 +1,33 @@
 #include <pebble.h>
 
-Window *s_window;
+enum
+{
+    MAX_STATION_NAME_LENGTH = 64,
+    MAX_DISTANCE_LENGTH = 32
+};
+
+Window *s_main_window;
 MenuLayer *s_menu_layer;
 MenuLayerCallbacks s_menu_callbacks;
 
-enum
-{
-    MAX_STATION_NAME_LENGTH = 64
-};
+Window *s_compass_window;
+GBitmap* s_compass_bitmap;
+RotBitmapLayer *s_compass_layer;
+TextLayer *s_station_name_layer;
+TextLayer *s_distance_layer;
+char s_distance_str[MAX_DISTANCE_LENGTH];
 
-struct Station
+typedef struct Station
 {
     char name[MAX_STATION_NAME_LENGTH];
     uint16_t distance;
-    int16_t heading;
+    CompassHeading heading;
     uint8_t bikes;
     uint8_t racks;
-} *s_stations = NULL;
+    uint16_t uid;
+} Station;
+Station *s_stations = NULL;
+Station *s_selected_station = NULL;
 int s_stations_length = 0;
 int s_number_of_stations = -1;
 
@@ -29,7 +40,20 @@ enum
     KEY_BIKES,
     KEY_RACKS,
     KEY_HEADING,
+    KEY_UID,
 };
+
+void update_compass_distance()
+{
+    snprintf(s_distance_str, MAX_DISTANCE_LENGTH, "%d meters", s_selected_station->distance);
+    text_layer_set_text(s_distance_layer, s_distance_str);
+}
+
+void update_compass_direction(CompassHeading heading)
+{
+    CompassHeading angle = (heading - s_selected_station->heading + TRIG_MAX_ANGLE) % TRIG_MAX_ANGLE;
+    rot_bitmap_layer_set_angle(s_compass_layer, angle);
+}
 
 // Called when a message is received from PebbleKitJS
 void inbox_received_callback(DictionaryIterator *iterator, void *context)
@@ -37,20 +61,29 @@ void inbox_received_callback(DictionaryIterator *iterator, void *context)
     Tuple *t = dict_read_first(iterator);
     while(t != NULL)
     {
-        struct Station *station = s_stations ? &s_stations[s_number_of_stations] : NULL;
+        Station *station = s_selected_station;
+        if (!station && s_stations)
+        {
+            station = &s_stations[s_number_of_stations];
+        }
+        
         switch (t->key)
         {
         case KEY_NUM_STATIONS:
             free(s_stations);
             s_number_of_stations = -1;
             s_stations_length = t->value->int32;
-            s_stations = calloc(s_stations_length, sizeof(struct Station));
+            s_stations = calloc(s_stations_length, sizeof(Station));
             break;
         case KEY_NAME:
             if (station) strncpy(station->name, t->value->cstring, MAX_STATION_NAME_LENGTH);
             break;
         case KEY_DISTANCE:
             if (station) station->distance = t->value->int32;
+            if (station == s_selected_station)
+            {
+                update_compass_distance();
+            }
             break;
         case KEY_BIKES:
             if (station) station->bikes = t->value->int32;
@@ -59,13 +92,22 @@ void inbox_received_callback(DictionaryIterator *iterator, void *context)
             if (station) station->racks = t->value->int32;
             break;
         case KEY_HEADING:
-            if (station) station->heading = t->value->int32;
+            if (station) station->heading = ((t->value->int32 + 360) % 360)*TRIG_MAX_ANGLE/360;
             break;
+        case KEY_UID:
+            if (station) station->uid = t->value->int32;
         }
         t = dict_read_next(iterator);
     }
-    s_number_of_stations++;
-    menu_layer_reload_data(s_menu_layer);
+    
+    if (!s_selected_station)
+    {   // menu is being updated
+        if (s_number_of_stations++ == menu_layer_get_selected_index(s_menu_layer).row)
+        {   // if selected item is updated, turn on lights
+            light_enable_interaction();
+        }
+        menu_layer_reload_data(s_menu_layer);
+    }
 }
 
 void inbox_dropped_callback(AppMessageResult reason, void *context)
@@ -83,7 +125,7 @@ void outbox_sent_callback(DictionaryIterator *iterator, void *context)
   APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
 
-uint16_t menu_get_num_rows(struct MenuLayer *menu_layer, uint16_t section_index, void *callback_context)
+uint16_t menu_get_num_rows(MenuLayer *menu_layer, uint16_t section_index, void *callback_context)
 {
     return s_stations_length == 0 ? 1 : s_stations_length;
 }
@@ -97,7 +139,7 @@ void menu_draw_row(GContext *ctx, const Layer *cell_layer, MenuIndex *cell_index
     else if (cell_index->row < s_number_of_stations)
     {   // row already loaded
         char buf[64];
-        struct Station *station = &s_stations[cell_index->row];
+        Station *station = &s_stations[cell_index->row];
         snprintf(buf, 64, "%dm, %d bikes/%d racks", station->distance, station->bikes, station->racks);
         menu_cell_basic_draw(ctx, cell_layer, station->name, buf, NULL);
     }
@@ -107,32 +149,96 @@ void menu_draw_row(GContext *ctx, const Layer *cell_layer, MenuIndex *cell_index
     }
 }
 
-void menu_select_click(struct MenuLayer *menu_layer, MenuIndex *cell_index, void *callback_context)
+void send_uid(int uid)
 {
-    APP_LOG(APP_LOG_LEVEL_INFO, "Item %d clicked!", cell_index->row);
-}
-
-void menu_select_long_click(struct MenuLayer *menu_layer, MenuIndex *cell_index, void *callback_context)
-{
-    APP_LOG(APP_LOG_LEVEL_INFO, "Item %d long clicked!", cell_index->row);
-
     DictionaryIterator *iter;
 	app_message_outbox_begin(&iter);
+    dict_write_int32(iter, KEY_UID, uid);
 	dict_write_end(iter);
   	app_message_outbox_send();
-    
+}
+
+void menu_select_click(MenuLayer *menu_layer, MenuIndex *cell_index, void *callback_context)
+{
+    APP_LOG(APP_LOG_LEVEL_INFO, "Item %d clicked!", cell_index->row);
+    window_stack_push(s_compass_window, true);
+}
+
+void menu_select_long_click(MenuLayer *menu_layer, MenuIndex *cell_index, void *callback_context)
+{
+    APP_LOG(APP_LOG_LEVEL_INFO, "Item %d long clicked!", cell_index->row);
     s_number_of_stations = 0;
     menu_layer_reload_data(s_menu_layer);
+    send_uid(0); // signal reload request
+}
+
+void compass_window_load()
+{
+    Layer *window_layer = window_get_root_layer(s_compass_window);
+    GRect bounds = layer_get_bounds(window_layer);
+    
+    s_station_name_layer = text_layer_create(GRect(0, 0, 144, 48));
+    text_layer_set_font(s_station_name_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+    text_layer_set_background_color(s_station_name_layer, GColorClear);
+    text_layer_set_text_color(s_station_name_layer, GColorWhite);
+    text_layer_set_text_alignment(s_station_name_layer, GTextAlignmentCenter);
+    text_layer_set_overflow_mode(s_station_name_layer, GTextOverflowModeTrailingEllipsis);
+    layer_add_child(window_layer, text_layer_get_layer(s_station_name_layer));
+    
+    s_compass_bitmap = gbitmap_create_with_resource(RESOURCE_ID_COMPASS_WHITE);
+    s_compass_layer = rot_bitmap_layer_create(s_compass_bitmap);
+    bitmap_layer_set_bitmap((BitmapLayer*)s_compass_layer, s_compass_bitmap);
+    GRect compass_frame = layer_get_frame((Layer*)s_compass_layer);
+    compass_frame.origin.x = (bounds.size.w-compass_frame.size.w)/2;
+    compass_frame.origin.y = 62;
+    layer_set_frame((Layer*)s_compass_layer, compass_frame);
+    layer_add_child(window_layer, bitmap_layer_get_layer((BitmapLayer*)s_compass_layer));
+    
+    s_distance_layer = text_layer_create(GRect(0, bounds.size.h-24, 144, 24));
+    text_layer_set_font(s_distance_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+    text_layer_set_background_color(s_distance_layer, GColorClear);
+    text_layer_set_text_color(s_distance_layer, GColorWhite);
+    text_layer_set_text_alignment(s_distance_layer, GTextAlignmentCenter);
+    layer_add_child(window_layer, text_layer_get_layer(s_distance_layer));
+}
+
+void compass_window_unload()
+{
+    text_layer_destroy(s_distance_layer);
+    rot_bitmap_layer_destroy(s_compass_layer);
+    gbitmap_destroy(s_compass_bitmap);
+    text_layer_destroy(s_station_name_layer);
+}
+
+void compass_handler(CompassHeadingData heading)
+{
+    update_compass_direction(heading.true_heading);
+}
+
+void compass_window_appear()
+{
+    s_selected_station = &s_stations[menu_layer_get_selected_index(s_menu_layer).row];
+    text_layer_set_text(s_station_name_layer, s_selected_station->name);
+    update_compass_distance();
+    send_uid(s_selected_station->uid); // signal selection
+    compass_service_subscribe(compass_handler);
+}
+
+void compass_window_disappear()
+{
+    compass_service_unsubscribe();
+    send_uid(0); // signal deselection
+    s_selected_station = NULL;
 }
 
 void init(void)
 {
-	s_window = window_create();
-	window_stack_push(s_window, true);
+	s_main_window = window_create();
+	window_stack_push(s_main_window, true);
     
-    Layer *window_layer = window_get_root_layer(s_window);
+    Layer *window_layer = window_get_root_layer(s_main_window);
 
-    // Create and Add to layer hierarchy
+    // Create and add to layer hierarchy
     s_menu_callbacks.get_num_rows = menu_get_num_rows;
     s_menu_callbacks.draw_row = menu_draw_row;
     s_menu_callbacks.select_click = menu_select_click;
@@ -142,7 +248,7 @@ void init(void)
     s_menu_layer = menu_layer_create(bounds);
     menu_layer_set_callbacks(s_menu_layer, NULL, s_menu_callbacks);
     layer_add_child(window_layer, menu_layer_get_layer(s_menu_layer));
-    menu_layer_set_click_config_onto_window(s_menu_layer, s_window);
+    menu_layer_set_click_config_onto_window(s_menu_layer, s_main_window);
 	
 	// Register AppMessage handlers
     app_message_register_inbox_received(inbox_received_callback);
@@ -151,13 +257,23 @@ void init(void)
     app_message_register_outbox_sent(outbox_sent_callback);
 		
 	app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+    
+    s_compass_window = window_create();
+    window_set_background_color(s_compass_window, GColorBlack);
+    window_set_window_handlers(s_compass_window, (WindowHandlers) {
+        .load = compass_window_load,
+        .appear = compass_window_appear,
+        .disappear = compass_window_disappear,
+        .unload = compass_window_unload
+    });
 }
 
 void deinit(void)
 {
+    window_destroy(s_compass_window);
 	app_message_deregister_callbacks();
     menu_layer_destroy(s_menu_layer);
-	window_destroy(s_window);
+	window_destroy(s_main_window);
     free(s_stations);
 }
 
--- src/mol_bubble.js
+++ src/mol_bubble.js
@@ -26,36 +26,45 @@ DistanceCalculator.prototype.setup = function(coords)
     
     this.lat = this.degToRad(coords.latitude);
     this.lon = this.degToRad(coords.longitude);
-    var  sin = Math.sin(this.lat);
+    this.sin = Math.sin(this.lat);
     this.cos = Math.cos(this.lat);
 
     // https://en.wikipedia.org/wiki/Earth_radius#Geocentric_radius
     var eec = equatR*equatR*this.cos;
-    var pps = polarR*polarR*sin;
-    this.R = Math.sqrt((eec*eec + pps*pps) / (eec*this.cos + pps*sin));
+    var pps = polarR*polarR*this.sin;
+    this.R = Math.sqrt((eec*eec + pps*pps) / (eec*this.cos + pps*this.sin));
 };
 DistanceCalculator.prototype.polar = function(coords)
 {
     // https://en.wikipedia.org/wiki/Geographical_distance#Spherical_Earth_projected_to_a_plane
-    var dlat = this.lat - this.degToRad(coords.lat);
-    var dlon = this.lon - this.degToRad(coords.lon);
+    var lat2 = this.degToRad(coords.lat);
+    var lon2 = this.degToRad(coords.lon);
+    var dlat = this.lat - lat2;
+    var dlon = this.lon - lon2;
     var cos_dlon = this.cos * dlon;
-    return {
-        "r": Math.round(this.R * Math.sqrt(dlat*dlat + cos_dlon*cos_dlon) * 1000),
-        "a": Math.atan2(dlat, this.cos*dlon)
-    };
+    var dist = Math.round(this.R * Math.sqrt(dlat*dlat + cos_dlon*cos_dlon) * 1000);
+    // http://www.movable-type.co.uk/scripts/latlong.html
+    var x = this.cos*Math.sin(lat2) - this.sin*Math.cos(lat2)*Math.cos(dlon);
+    var y = Math.sin(dlon) * Math.cos(lat2);
+    var bearing = Math.atan2(y,x);
+    return { "r": dist, "a": bearing };
 };
 
 var stations = [];
 var stations_to_send = 0;
 var next_station_to_send = 0;
+var selected_uid = 0;
+var station_list_dirty = false;
 var dc = new DistanceCalculator();
 
 function appMessageSent(e)
 {
     console.log("Station sent to Pebble successfully!");
-    next_station_to_send++;
-    sendNextStation();
+    if (selected_uid === 0)
+    {
+        next_station_to_send++;
+        sendNextStation();
+    }
 }
 
 function appMessageError(e)
@@ -70,6 +79,7 @@ function sendNextStation()
     {
         var station = stations[next_station_to_send];
         Pebble.sendAppMessage({
+            "uid": station.uid,
             "name": station.name,
             "distance": station.dist,
             "heading": dc.radToDeg(station.heading),
@@ -84,18 +94,31 @@ function updateStations()
     if (stations.length === 0 || isNaN(dc.lat) || isNaN(dc.lon))
         return;
     
+    next_station_to_send = -1;
     for (var i = 0; i < stations.length; i++)
     {
         var station = stations[i];
         var polar = dc.polar(station);
         station.dist = polar.r;
         station.heading = polar.a;
+        if (station.uid == selected_uid)
+        {
+            next_station_to_send = i;
+        }
     }
-    stations.sort(function (a,b) { return a.dist - b.dist; });
 
-    next_station_to_send = -1;
-    stations_to_send = Math.min(stations.length, 10);
-    Pebble.sendAppMessage({"num_stations":stations_to_send}, appMessageSent);
+    if (next_station_to_send == -1)
+    {   // send all stations
+        stations.sort(function (a,b) { return a.dist - b.dist; });
+        stations_to_send = Math.min(stations.length, 10);
+        Pebble.sendAppMessage({"num_stations":stations_to_send}, appMessageSent);
+        station_list_dirty = false;
+    }
+    else
+    {   // send distance and heading for selected station
+        sendNextStation();
+        station_list_dirty = true;
+    }
 }
 
 function locationSuccess(pos)
@@ -126,6 +149,7 @@ function fetchStationList()
             {
                 var place = places[j];
                 stations.push({
+                    "uid": parseInt(place.getAttribute("uid")),
                     "name": place.getAttribute("name").slice(5),
                     "lat": parseFloat(place.getAttribute("lat")),
                     "lon": parseFloat(place.getAttribute("lng")),
@@ -151,5 +175,16 @@ Pebble.addEventListener('ready', function(e)
 Pebble.addEventListener('appmessage', function(e)
 {
     console.log("AppMessage received!");
-    fetchStationList();
+    if (e.payload.uid === 0 && selected_uid === 0)
+    {   // refresh station list
+        fetchStationList();
+    }
+    else
+    {   // select or deselect station
+        selected_uid = e.payload.uid;
+        if (selected_uid === 0 && station_list_dirty)
+        {
+            updateStations();
+        }
+    }
 });
